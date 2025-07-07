0812a88f99dae6a4bbeac5f037fe4b61bce58ddd
Emritz <krishtoolmain@gmail.com>
2025-07-07 18:04:27 +0800

    Update chanxreyno2.py

--- chanxreyno2.py
+++ chanxreyno2.py
@@ -3,7 +3,7 @@ from time import sleep
 
 BASE_URL: str = "https://admincpm.io/chanxreyno2/api"
 
-class CPMNuker:
+class Emritz:
 
     def __init__(self, access_key) -> None:
         self.auth_token = None
