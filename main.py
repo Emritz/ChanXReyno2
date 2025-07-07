774ac71b5babdf9a787f315092b84f7b207dcf48
Emritz <krishtoolmain@gmail.com>
2025-07-07 18:50:36 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -37,7 +37,7 @@ def gradient_text(text, colors):
 
 def banner(console):
     os.system('cls' if os.name == 'nt' else 'clear')
-    brand_name = figlet_format('AmerCPM2', font='drpepper')
+    brand_name = figlet_format('ChanXReyno2', font='drpepper')
     colors = [
         "rgb(255,0,0)", "rgb(255,69,0)", "rgb(255,140,0)", "rgb(255,215,0)", "rgb(173,255,47)", 
         "rgb(0,255,0)", "rgb(0,255,255)", "rgb(0,191,255)", "rgb(0,0,255)", "rgb(139,0,255)",
@@ -45,7 +45,7 @@ def banner(console):
     ]
     colorful_text = gradient_text(brand_name, colors)
     console.print(colorful_text, end=None)
-    console.print("[bold green]♕ AmerCPM2[/bold green]: Car Parking Multiplayer 2 Hacking Tool.")
+    console.print("[bold green]♕ ChanXReyno2[/bold green]: Car Parking Multiplayer 2 Hacking Tool.")
     console.print(f"[bold green]♕ Telegram[/bold green]: [bold blue]@{__CHANNEL_USERNAME__}[/bold blue] or [bold blue]@{__GROUP_USERNAME__}[/bold blue].")
     console.print("[bold red]==================================================[/bold red]")
     console.print("[bold yellow]! Note[/bold yellow]: Logout from the game before using this tool !.", end="\n\n")
@@ -184,7 +184,7 @@ if __name__ == "__main__":
                 if status == 0:
                     console.print("[bold green]SUCCESSFUL.[/bold green]")
                     console.print("==================================")
-                    console.print(f"[bold red]! INFO[/bold red]: In order to tweak this account with AmerCPM2")
+                    console.print(f"[bold red]! INFO[/bold red]: In order to tweak this account with ChanXReyno2")
                     console.print("you most sign-in to the game using this account.")
                     sleep(2)
                     continue
