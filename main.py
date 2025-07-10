0e8f8bd7b8ac7f73bd5e01acdaab562e6afe1f72
Emritz <krishtoolmain@gmail.com>
2025-07-10 09:35:02 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -160,7 +160,6 @@ if __name__ == "__main__":
             console.print("[bold][cyan](11):[/cyan] [green]Unlock All Clothes ~ 9K[/green]")
             console.print("[bold][cyan](12):[/cyan] [green]Unlock All Calipers ~ 5K[/green]")
             console.print("[bold][cyan](13):[/cyan] [green]Unlock All Paints ~ 7K[/green]")
-            console.print("[bold][cyan](14):[/cyan] [green]Unlock Cars ~ 10K[/green]")
             console.print("[bold][cyan](0) :[/cyan] [red]Exit[/red]", end="\n\n")
             service = IntPrompt.ask(f"[bold][?] Select a Service [red][1-{choices[-1]} or 0][/red][/bold]", choices=choices, show_choices=False)
             if service == 0: # Exit
