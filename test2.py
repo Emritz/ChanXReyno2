22c6184a6519eefd16d4ff9cca69cf0995ce539e
Emritz <krishtoolmain@gmail.com>
2025-07-12 13:15:29 +0800

    Update test2.py

--- test2.py
+++ test2.py
@@ -7,7 +7,7 @@ import os, signal, sys
 import subprocess
 import progressbar
 from getpass import getpass
-from cybercpm import CyberCPM
+from chanxreyno2 import Emritz
 from pystyle import Add, Center, Anime, Colors, Colorate, Write, System
 from colorama import Fore, Style, init
 from pystyle import System as pySystem
@@ -171,7 +171,7 @@ if __name__ == "__main__":
         acc_password = prompt_valid_value(f"{Fore.CYAN}  ---[{Style.RESET_ALL} Password", "Password", False)
         acc_access_key = prompt_valid_value(f"{Fore.CYAN}  ---[{Style.RESET_ALL} Access Key", "Access Key", False)
         print(f"{Fore.CYAN}  ---[{Style.RESET_ALL} Attempting login... ", end="")
-        cpm = CyberCPM(acc_access_key)
+        cpm = Emritz(acc_access_key)
         login_response = cpm.login(acc_email, acc_password)
 
         if login_response != 0:
