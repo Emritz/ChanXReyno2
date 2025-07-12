ee864308ec8eb276786a0b0f280441e4febeec3f
Emritz <krishtoolmain@gmail.com>
2025-07-12 09:35:37 +0800

    Update main.py

--- main.py
+++ main.py
@@ -396,7 +396,8 @@ if __name__ == "__main__":
                     console.print("==================================")
                     answ = Prompt.ask("[bold cyan][?] Do You want to Exit ?[/bold cyan]", choices=["y", "n"], default="n")
                     if answ == "y":
-                    console.print(f"[bold yellow][!] Thank You for using our tool, please join our telegram channel[/bold yellow]: [bold blue]@{__CHANNEL_USERNAME__}[/bold blue].")
+                    console.print(f"[bold yellow][!] Thank You for using our tool, please join our telegram channel[/bold yellow]: [bold blue]@{__CHANNEL_USERNAME__}[/bo
+                    ld blue].")
 else:
     continue
                     console.print("[bold red]FAILED.[/bold red]")
